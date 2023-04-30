0f20691d67b721ff6d426571b909aaa0ece77b9c
Paolo Mostardi <paolomostardi@gmail.com>
2023-04-30 09:34:04 +0100

    backend feature: function can now recreate model with the id.

--- Backend/model_evolution.py
+++ Backend/model_evolution.py
@@ -113,3 +113,52 @@ def random_generation(number_of_cycles,fit,test):
 
     return model_list
 
+
+def generate_model_from_id(model_id, input_shape):
+
+    X = Conv2D(filters= model_id[0][0][0], kernel_size=model_id[0][0][1],activation='relu')(input_shape)
+
+    for block in model_id[1:-2]:
+
+        residual_choice = block[0]
+        if residual_choice:
+            X_shortcut = X
+
+        filter_choice, kernel_choice = block[1]
+        X = Conv2D(filters=filter_choice, kernel_size=kernel_choice, activation='relu',padding='same')(X)
+
+        filter_choice, kernel_choice = block[2]
+        X = Conv2D(filters=filter_choice, kernel_size=kernel_choice, activation='relu',padding='same')(X)
+
+        if residual_choice:
+            X_shortcut = Conv2D(filters=filter_choice,kernel_size=(1,1),activation='relu',padding='same')(X_shortcut)
+            X_shortcut = Conv2D(filters=filter_choice, kernel_size=(1, 1), activation='relu',padding='same')(X_shortcut)
+            X = Add()([X, X_shortcut])
+
+        pooling_choice = block[3]
+
+        if pooling_choice:
+            X = MaxPooling2D((2, 2), padding='same')(X)
+
+        print(block[4])
+
+        normalization_choice = block[4]
+        if normalization_choice:
+            X = BatchNormalization(axis=1)(X)
+
+    print('Flatten')
+    X = Flatten()(X)
+
+    dense_choice = model_id[-2]
+    print(dense_choice)
+    X = Dense(dense_choice, 'relu')(X)
+    dense_choice = model_id[-1]
+    X = Dense(dense_choice, 'relu')(X)
+    from_output_layer = Dense(2*8*8, activation='softmax')(X)
+    from_output_layer = Reshape((2, 8, 8))(from_output_layer)
+    model = Model(inputs=input_shape, outputs=[from_output_layer])
+    opt = SGD(lr=0.01, momentum=0.9)
+    model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
+    return model
+
+
