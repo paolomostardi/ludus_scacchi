7d98736defab74a40c8b1fa5b41b75edf5a8d3d7
paolo <paolomostardi@gmail.com>
2023-10-17 18:40:00 +0200

    Backend: adding resnet to the architecture

--- Backend/models/training_notebook.py
+++ Backend/models/training_notebook.py
@@ -1,24 +1,47 @@
-
-print('LOADING LIBRARIES')
-
 import numpy as np
-
 from keras import backend as K
 import gc
-
 from keras.models import Model
-
 from keras.layers import Conv2D, Reshape, MaxPooling2D
 from keras.layers import Dense, ZeroPadding2D
 from keras.layers import Flatten, Concatenate, BatchNormalization
 from keras import Input
-
 from keras.regularizers import l2
-
 from keras.optimizers import SGD
+from keras.layers import Add
+from keras.layers import Activation
 
+print('LIBRARIES LOADED')
 
+def test_resnet():
+    x1 = (1, 1)
+    x3 = (3, 3)
 
+    input_shape = (14, 8, 8)
+    input_layer = Input(shape=input_shape, name='input_1')
+
+    conv1 = Conv2D(256, kernel_size=x3, strides=(1, 1), padding='same', activation='relu')(input_layer)
+    conv2 = Conv2D(256, kernel_size=x3, strides=(1, 1), padding='same', activation='relu')(conv1)
+    conv3 = Conv2D(128, kernel_size=x1, strides=(1, 1), padding='same', activation='relu')(conv2)
+
+    pool1 = MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding='same')(conv3)
+    pad1 = ZeroPadding2D(padding=(1, 1))(pool1)  # Adjusted padding to ensure compatibility
+
+    conv4 = Conv2D(128, kernel_size=x1, strides=(1, 1), padding='same', activation='relu')(pad1)
+    conv5 = Conv2D(128, kernel_size=x3, strides=(1, 1), padding='same', activation='relu')(conv4)
+    skip_connection = Add()([conv5, pad1])
+
+    flatten = Flatten(name='flatten')(skip_connection)
+    dense1 = Dense(64, activation='relu', name='dense')(flatten)
+    dense2 = Dense(32, activation='relu', name='dense_1')(dense1)
+    dense3 = Dense(64, activation='softmax', name='dense_2')(dense2)
+
+    model = Model(inputs=[input_layer], outputs=[dense3])
+
+    opt = SGD(learning_rate=0.01, momentum=0.9)
+    model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
+
+    return model
 
 
 def final_model_architecture():
@@ -58,11 +81,10 @@ def final_model_architecture():
     flatten = Flatten(name='flatten')(pool3)
     dense1 = Dense(64, activation='relu', name='dense')(flatten)
     dense2 = Dense(32, activation='relu', name='dense_1')(dense1)
-    dense3 = Dense(128, activation='relu', name='dense_2')(dense2)
+    dense3 = Dense(64, activation='softmax', name='dense_2')(dense2)
 
-    reshape_layer = Reshape(target_shape=(2, 8, 8), name='reshape')(dense3)
+    model = Model(inputs=[input_layer], outputs=[dense3])
 
-    model = Model(inputs=input_layer, outputs=[reshape_layer])
     opt = SGD(learning_rate=0.01, momentum=0.9)
     model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
     return model
@@ -86,7 +108,6 @@ def double_input_shape_model():
     input_shape = (14, 8, 8)
 
     input_layer = Input(shape=input_shape, name='input_1')
-
 
     conv1 = Conv2D(256, kernel_size=x3, strides=(1, 1), padding='valid', activation='relu',
                    kernel_regularizer=l2(weight_decay))(input_layer)
@@ -205,7 +226,7 @@ def change_data():
 
 print('LOADING DATA')
 
-model_to_fit = double_input_shape_model()
+model_to_fit = test_resnet()
 x = np.load('/kaggle/input/1700-lichess-user-bitboards/x.npy', mmap_mode='r')
 y = np.load('/kaggle/input/1700-lichess-user-bitboards/asdad.npy', mmap_mode='r')
 white = np.load('/kaggle/input/1700-lichess-user-bitboards/white2.npy', mmap_mode='r')
