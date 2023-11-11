031c541f404f62a79fe6f4637ff0570ff6e34bbb
paolo <paolomostardi@gmail.com>
2023-11-11 15:14:24 +0100

    creating the first part of the engine

---
+++ Backend/engine__creation/engine_creation.py
@@ -0,0 +1,49 @@
+import chess
+import numpy as np
+
+from keras import models
+from keras.models import load_model
+from Backend.pipeline import from_PGN_generate_bitboards as gen 
+
+"""
+
+63 62 61 60 59 58 57 56
+55 54 53 52 51 50 49 48
+47 46 45 44 43 42 41 40
+39 38 37 36 35 34 33 32
+31 30 29 28 27 26 25 24
+23 22 21 20 19 18 17 16
+15 14 13 12 11 10 9  8
+7  6  5  4  3  2  1  0
+
+"""
+
+
+def return_top_piece_to_move(fen: str, model: models.Model):
+    board = chess.Board(fen)
+    x = gen.from_chess_board_create_bit_boards(board)
+    x = x.reshape(1, 14, 8, 8)
+    prediction = model.predict(x)
+
+    sorted_indices = np.argsort(prediction[0])[::-1]
+    sorted_percentages = prediction[0][sorted_indices] * 100
+
+    return sorted_indices, sorted_percentages
+
+def has_legal_moves(board, square):
+    piece = board.piece_at(square)
+    if piece is None:
+        return False 
+    for move in board.legal_moves:
+        if move.from_square == square:
+            return True 
+
+    return False 
+
+def return_best_legal_piece(board: chess.Board, model: models.Model):
+    top_indices, _ = return_top_piece_to_move(board.fen(), model)
+    for piece in top_indices:
+        if has_legal_moves(board, piece):
+            return piece
+
+
--- Backend/evaluation/generate_evaluation_dataset.py
+++ Backend/evaluation/generate_evaluation_dataset.py
@@ -3,19 +3,9 @@ from Backend.pipeline import from_PGN_generate_bitboards as gen
 from keras.models import Model
 import numpy as np
 
-def from_fen_to_answer(fen: str, model : Model):
-    board = chess.Board(fen)
-    x = gen.from_chess_board_create_bit_boards(board)
-    x = x.reshape(1,14,8,8)
-    prediction = model.predict(x)
 
-    top3_indices = np.argsort(prediction[0])[-3:][::-1]
-    top3_percentages = prediction[0][top3_indices] * 100
-
-    return top3_indices, top3_percentages
 
 def generate_first_dataset():
-    
     return 
 
 def make_0_63(couple):
@@ -25,7 +15,6 @@ def create_array(index):
     array = np.zeros(64)
     array[index] = 1
     return array
-
 
 
 def change_y(y):
