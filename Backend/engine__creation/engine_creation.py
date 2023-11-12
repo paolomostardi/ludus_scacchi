e737a855e398d0d0820006615cd04bc68277ec84
paolo <paolomostardi@gmail.com>
2023-11-12 14:50:29 +0100

    fixing some bugs in the second part of the engine

--- Backend/engine__creation/engine_creation.py
+++ Backend/engine__creation/engine_creation.py
@@ -31,19 +31,29 @@ def return_top_piece_to_move(fen: str, model: models.Model):
     return sorted_indices, sorted_percentages
 
 def has_legal_moves(board, square):
+
+    from_square = square
+    from_square = (from_square - (from_square % 8)) + (7 - from_square % 8)
+
+    square = from_square
+
     piece = board.piece_at(square)
     if piece is None:
         return False 
     for move in board.legal_moves:
         if move.from_square == square:
+            print(move)
+            print(move)
             return True 
 
     return False 
 
-def return_best_legal_piece(board: chess.Board, model: models.Model):
-    top_indices, _ = return_top_piece_to_move(board.fen(), model)
+def return_best_legal_piece(fen, model: models.Model):
+    top_indices, _ = return_top_piece_to_move(fen, model)
+    board = chess.Board(fen)
     for piece in top_indices:
         if has_legal_moves(board, piece):
+            print(piece)
             return piece
 
 
@@ -61,12 +71,14 @@ def transorm_index_to_matrix(index: int):
 
     matrix[0][y][x] = 1
 
+    print(matrix)
+
     return matrix
+
 
 def return_top_squares_given_square_and_board(fen: str, model: models.Model, square: chess.square):
     board = chess.Board(fen)
-    x = gen.from_chess_board_create_bit_boards(board)
-   
+    x = gen.from_chess_board_create_bit_boards(board)   
 
     matrix = transorm_index_to_matrix(square)
     x = np.concatenate((x, matrix), axis=0)
@@ -78,6 +90,39 @@ def return_top_squares_given_square_and_board(fen: str, model: models.Model, squ
 
     return sorted_indices, sorted_percentages
 
+
+def legal_move_given_square(board : chess.Board, from_square, to_square):
+
+    
+    from_square = (from_square - (from_square % 8)) + (7 - from_square % 8)
+    for move in board.legal_moves:
+        
+        
+        if move.from_square == from_square and move.to_square == to_square:
+            
+            print(from_square)
+            print(move)
+            print(board)
+            board.push(move)
+            print(board)
+            return True       
+
+
+def find_most_likely_legal_move(board, from_square, list_squares):
+
+    for to_square in list_squares:
+        if legal_move_given_square(board, from_square, to_square):
+            return to_square 
+
+
 def return_square_to_move(fen : str, model: models.Model, square: chess.square):
-    return_top_squares_given_square_and_board(fen,model,square)
+    sorted_indices, sorted_percentages = return_top_squares_given_square_and_board(fen,model,square)
+    return find_most_likely_legal_move(chess.Board(fen),square,sorted_indices)
+
+
+def engine(fen: str, model1, model2):
+    square = return_best_legal_piece(fen,model1)
+    print('INITIAL from square is ',square)
+
+    return return_square_to_move(fen,model2,square)
 
