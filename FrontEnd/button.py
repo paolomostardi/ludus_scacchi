3a3a75aac30973b89a698f9de10d07dd50cd6317
paolo <paolomostardi@gmail.com>
2023-11-15 16:26:05 +0100

    giving button update message function and border

--- FrontEnd/button.py
+++ FrontEnd/button.py
@@ -12,6 +12,8 @@ class Button:
         self.text_surface = self.font.render(self.message, True, (0,0,0))
 
         self.on_click = None
+        self.outside = None
+        self.outside_color = color
         self.screen = screen 
 
     def set_on_click(self, method):
@@ -21,9 +23,21 @@ class Button:
         click_x, click_y = coordinates
         if self.x <= click_x <= self.x + self.width and self.y <= click_y <= self.y + self.height:
             return True
+    
+    def set_outside(self, outside, color = (100,100,100)):
+        self.outside = outside
+        self.outside_color = color
+
+    def update_message(self,message):
+        self.message = message
+        self.text_surface = self.font.render(message, True, (0,0,0))
+
 
     def render(self):
         # pygame.draw.rect(screen,(0,0,0),random_choice_square[1])
         
         pygame.draw.rect(self.screen, self.color, self.rectangle)
         self.screen.blit(self.text_surface, (self.x , self.y + self.height // 2 ))
+
+        if self.outside:
+            pygame.draw.rect(self.screen, self.outside_color, self.rectangle, 2)
