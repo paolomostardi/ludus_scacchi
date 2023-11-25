ba1bf6221fe7d34afb0531bd53610b74d48453a2
paolo <paolomostardi@gmail.com>
2023-11-25 18:31:30 +0100

    analysis_mode now shows engine suggested move

--- FrontEnd/analysis_mode.py
+++ FrontEnd/analysis_mode.py
@@ -1,3 +1,4 @@
+from Backend.engine__creation import engine_creation as engine
 from FrontEnd import helper
 from FrontEnd.render_chess import RenderChess
 from FrontEnd.analysis_logic import AnalysisLogic
@@ -5,6 +6,8 @@ from FrontEnd.button import Button
 
 import pygame
 import chess
+
+from keras.models import load_model
 
 import chess.engine
 
@@ -17,23 +20,34 @@ class AnalysisMode(RenderChess):
         move_rectangle = (800,150,350,620)
         self.move_background = Button(move_rectangle,color=(106,131,146),screen=screen, border_radius=15)
 
-        print('hello')
+        engine_name_rectangle = (800,30,200,50)
+        self.engine_name_button = Button(engine_name_rectangle,color=(77,96,107),screen=screen,padding=True,padding_size=1, font_size=40,padding_color=(50,50,50), message='Engine 1')
 
-        if logic_board is None:
-            print('nothing here')
-            self.logic_board = AnalysisLogic()
-            self.list_of_moves = []
+        change_model_rectangle = (1000,30,150,50)
+        self.change_model_button = Button(change_model_rectangle,color=(106,131,146),screen=screen,padding=True,padding_size=1, font_size=20,padding_color=(50,50,50), message='Change model',font_padding=(5,10))   
 
-        else:
-            print('here we go')
-            self.logic_board = logic_board
-            self.chess_board = logic_board.get_current_board()
-            self.list_of_moves = []
-            for move in list_of_moves:
-                self.append_move_to_list_of_moves_to_render(move)
-            self.chess_board = logic_board.get_current_board()   
-            print('DONE YEYEYEYY')
-          
+        best_move_rectangle = (800,80,350,50)
+        self.best_move_button = Button(best_move_rectangle,color=(106,131,146),screen=screen,padding=True,padding_size=1,padding_color=(50,50,50))       
+
+        self.logic_board = logic_board
+        self.chess_board = logic_board.get_current_board()
+        self.list_of_moves = []
+        for move in list_of_moves:
+            self.append_move_to_list_of_moves_to_render(move)
+        self.chess_board = logic_board.get_current_board()
+
+        model_path = r'C:\Users\paolo\OneDrive\Desktop\Final_project\Ludus_scacchi\training_data\model\resnet_vgg_1700'
+        self.model1 = load_model(model_path + r'\first_part\model.h5')
+        self.model2 = load_model(model_path + r'\second_part\model.h5')
+        self.calculate_best_move()
+
+    def calculate_best_move(self):
+        move = engine.engine(self.chess_board.fen(),self.model1,self.model2)
+        self.best_move = move
+        self.best_move_button.update_message(self.chess_board.san(move))
+        print('updating the best move ', move)
+        return move
+      
     def append_move_to_list_of_moves_to_render(self, move):
         
         index = len(self.list_of_moves)
@@ -42,7 +56,7 @@ class AnalysisMode(RenderChess):
         else:
             square = (800,int(index/2) * 50 + 150, 175, 50)
 
-        self.list_of_moves.append(Button(square, (95,125,140),self.screen,message=move,font_padding=(60,0),padding=False,padding_size=2, font_size=45,padding_color=(80,100,150)))
+        self.list_of_moves.append(Button(square, (95,125,140),self.screen,message=move,font_padding=(60,0),padding=True,padding_size=1, font_size=45,padding_color=(50,50,50)))
         
     def generate_move(self):
         try:
@@ -78,13 +92,17 @@ class AnalysisMode(RenderChess):
             self.append_move_to_list_of_moves_to_render(self.chess_board.san(move))
             self.chess_board.push(move)
 
+        self.calculate_best_move()
+
     def key_up(self):
         self.logic_board.key_up()
         self.chess_board = self.logic_board.get_current_board()
+        self.calculate_best_move()
 
     def key_down(self):
         self.logic_board.key_down()
         self.chess_board = self.logic_board.get_current_board()
+        self.calculate_best_move()
 
     def render_move_list(self):
         self.move_background.render()
@@ -93,6 +111,12 @@ class AnalysisMode(RenderChess):
 
         return
 
+    def render_best_move(self):
+
+        self.engine_name_button.render()
+        self.change_model_button.render()
+        self.best_move_button.render()
+        return
 
 def main(logic_board: AnalysisLogic = None, list_of_moves = []):
 
@@ -147,6 +171,7 @@ def main(logic_board: AnalysisLogic = None, list_of_moves = []):
         background_button.render()
         board.render_board()
         board.render_move_list()
+        board.render_best_move()
 
         pygame.display.update()
         clock.tick(framerate)
