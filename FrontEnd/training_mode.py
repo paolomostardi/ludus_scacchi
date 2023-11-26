9923d2fc6e0d39951654d014e2ab844335c05a94
paolo <paolomostardi@gmail.com>
2023-11-26 18:34:06 +0100

    basic FrontEnd for training_mode

--- FrontEnd/training_mode.py
+++ FrontEnd/training_mode.py
@@ -10,49 +10,8 @@ from keras.models import load_model
 from keras import Model
 import pandas as pd
 import os
+from FrontEnd import helper
 
-def choose_model():
-
-    WIDTH = 1200
-    HEIGHT = 800
-
-    running = True
-    pygame.font.init() 
-    clock = pygame.time.Clock()
-    framerate = 15
-    screen = pygame.display.set_mode((WIDTH, HEIGHT))
-
-    analysis_rect = (100,50,1000,150)
-    question_button = Button(analysis_rect, color= (30,123,45), message = '  Choose the model you would like to train' , screen=screen )
-
-    analysis_rect = (100,350,500,150)
-    vgg_button = Button(analysis_rect, color= (123,123,45), message = ' VGG ' , screen=screen )
-    
-    analysis_rect = (600,350,500,150)
-    resnet_button = Button(analysis_rect, color= (30,123,123), message = ' RESNET ' , screen=screen )
-    
-    while running:
-
-        for event in pygame.event.get():
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                print('CLICK EVENT COORDINATE ')
-                print(event.pos)
-                if vgg_button.check_click(event.pos):
-                    return
-                if resnet_button.check_click(event.pos):
-                    return 
-
-            if event.type == pygame.QUIT:
-                    running = False
-
-        screen.fill((150, 150, 150))
-        
-        question_button.render()
-        vgg_button.render()
-        resnet_button.render()
-
-        pygame.display.update()
-        clock.tick(framerate)
 
 def choose_user():
 
@@ -65,17 +24,49 @@ def choose_user():
     framerate = 15
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
-    user_text = ''
-    text_box_clicked = False
+    background_color = (33,41,46)
+    black = (50,50,50)
+    padding_size = 2
+    button_gray = (200,200,200)
+    light_blue = (106,131,146)
+    orange = (218,145,37)
 
-    analysis_rect = (100,50,1000,150)
-    question_button = Button(analysis_rect, color= (30,123,45), message = '  Choose the user you would like to train ' , screen=screen )
+    user_text = ''
+    text_box_clicked = False    
     
-    analysis_rect = (100,300,1000,150)
-    user_input = Button(analysis_rect, color= (30,123,45), message = user_text , screen=screen )
+    background_div_rect = (0,0,1200,550)
+    background_div = Button(background_div_rect, color = light_blue, screen=screen )    
     
-    analysis_rect = (150,550,300,200)
-    enter_button = Button(analysis_rect, color= (200,45,45), message = ' Confirm user ' , screen=screen )
+    select_model_rect = (20,50,500,50)
+    select_model_message = Button(select_model_rect, color= orange, message = ' Select model ' ,font_size = 32, font_padding=(10,10), screen=screen, border_radius=10, padding=True, padding_color=black,padding_size=padding_size )
+
+    engine_button_list = []
+
+    path = 'training_data\model'
+    for index, engine_name in enumerate(os.listdir(path)):
+        engine_rect = (40,120 + (50 * index), 350, 35) 
+        engine_button_list.append( Button(engine_rect,orange,screen,message=engine_name,font_size= 27, font_padding=(20,-2),padding=True,padding_size=padding_size,padding_color=(50,50,50) ) )    
+    
+    select_user_rect = (600,50,500,50)
+    select_user_message = Button(select_user_rect, color = orange, message = ' Insert name of the user ' ,font_size = 32, font_padding=(10,10), screen=screen, border_radius=10, padding=True, padding_color=black,padding_size=padding_size )
+    
+    user_input_rect = (610,120,400,45)
+    user_input = Button(user_input_rect, color= orange, message = user_text , screen=screen , padding=True, padding_color=black, padding_size=padding_size, font_size = 25 , font_padding=(10,5))
+    
+    user_search_rect = (920,120,200,45)
+    user_search_button = Button(user_search_rect, color= button_gray, message = 'Search user' , screen=screen , padding=True, padding_color=black, padding_size=padding_size, font_size = 25 , font_padding=(45,10))
+
+    train_model_rect = (20,470,250,70)
+    train_model_button = Button(train_model_rect, color= orange, message = 'TRAIN MODEL' , screen=screen, font_size= 25 ,padding=True, padding_color=black, padding_size= padding_size, border_radius=10, font_padding=(10,10))
+
+    new_model_rect = (300,470,250,70)
+    new_model_button = Button(new_model_rect, color= orange, message = 'NEW MODEL' , screen=screen, font_size= 25 ,padding=True, padding_color=black, padding_size= padding_size, border_radius=10, font_padding=(10,10))
+ 
+    
+    
+    flag_image = pygame.image.load(r'FrontEnd\Pieces\search_icon.png')
+    
+    flag_image = helper.resize_image(flag_image, 200, 0.18)
 
     while running:
 
@@ -88,7 +79,7 @@ def choose_user():
                     text_box_clicked = True
                 else:
                     text_box_clicked = False
-                if enter_button.check_click(event.pos):
+                if train_model_button.check_click(event.pos):
                     user = get_user_info(user_text)
                     if user:
                         return user
@@ -108,11 +99,25 @@ def choose_user():
 
                 print(user_input.message)
 
-        screen.fill((150, 150, 150))
+        screen.fill(background_color)
+        background_div.render()
+        select_model_message.render()
+        select_user_message.render()
+
         
-        question_button.render()
+        
         user_input.render()
-        enter_button.render()
+        user_search_button.render()
+        train_model_button.render()
+        new_model_button.render()
+
+        screen.blit(flag_image, (user_search_button.x + 5, user_search_button.y + 5))
+        
+
+
+
+        for i in engine_button_list:
+            i.render()
 
         pygame.display.update()
         clock.tick(framerate)
@@ -296,9 +301,8 @@ def train_model_given_user_and_model(username, path_to_model, path_to_save = Non
 
 
 def main():
-    model_path = 'training_data\model\model1'
-    username = 'chess'
-    train_model_given_user_and_model(username,model_path)
+
+    choose_user()
 
 
 
